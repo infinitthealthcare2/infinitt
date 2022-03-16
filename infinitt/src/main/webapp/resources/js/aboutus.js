3dccf71177bc836793f087bcb03d7359a5276adc
Owner <Owner@LAPTOP-3J76CER1>
2022-03-16 13:58:00 +0900

    ss

--- infinitt/src/main/webapp/resources/js/aboutus.js
+++ infinitt/src/main/webapp/resources/js/aboutus.js
@@ -3,24 +3,23 @@ $(document).ready(function(){
 
 
 
-	/*setInterval(() => {
-
-	    //window.scrollTo(0, -document.body.scrollHeight, "smooth")
-	    //window.scrollTop();
-	    window.scrollTo(0, document.body.scrollHeight, "smooth");
-	    
-	  },32);*/
+//	setInterval(() => {
+//
+//	    //window.scrollTo(0, -document.body.scrollHeight, "smooth")
+//	    //window.scrollTop();
+//	    window.scrollTo(0, document.body.scrollHeight, "smooth");
+//	    
+//	  },32);
 	
 	
 
 
 })
 
-/*p = 0; //초기 포커싱위치를 0으로 설정해주도록한다
+p = 0; //초기 포커싱위치를 0으로 설정해주도록한다
 function scrolling() {
-    window.scroll(10,p); //스크롤바의 위치 설정
+    window.scroll(0,p); //스크롤바의 위치 설정
     p = p + 2; //y의 값을 점점 내리도록한다
-    if(p ==70000) p = 0; //만약 y축의 값이 70000이 되면 초기화를 하도록설정
-    setTimeout("scrolling()",10); //0.1초마다 이 함수를 반복하도록설정
+    setTimeout("scrolling()",20); //0.1초마다 이 함수를 반복하도록설정
 }
-scrolling();*/
+scrolling();
